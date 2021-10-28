9ae61f9c4807d3a9a4d7b88a39badc19686948b8
Nishu Tyagi <tyaginishu7@gmail.com>
2021-10-28 22:15:19 +0530

    [Nishu Tyagi]Refactored:Code to add Side MenuItems

--- src/Components/BooksDisplay/BooksDisplay.jsx
+++ src/Components/BooksDisplay/BooksDisplay.jsx
@@ -1,17 +1,72 @@
 import React from 'react';
 import Books from './Books';
 import './BooksDisplay.scss';
-
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ExpandMore from '@material-ui/icons/ExpandMore';
 
 const BooksDisplay = (props) => { 
+
     const bookList = props.bookarr.map((info) => <Books info={info} booksDisplay ={props.booksDisplay}/>);
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div>
+            <div>
+                <Button
+                    className="sort"
+                    id="demo-positioned-button"
+                    aria-controls="demo-positioned-menu"
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}
+                >
+                    Sort by relevance
+                    <ExpandMore/>
+                </Button>
+                <Menu
+                    id="demo-positioned-menu"
+                    aria-labelledby="demo-positioned-button"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>Price : Low to High</MenuItem>
+                    <MenuItem onClick={handleClose}>Price : High to Low</MenuItem>
+                    <MenuItem onClick={handleClose}>Newest Arrivals</MenuItem>
+                </Menu>
+            </div>
             <div className="book_header">
                 <h2 className="book_name">Books</h2>
                 <p className="header_txt">(128 items)</p>
             </div>
+
             <div className="bookdisplay_main">{bookList}</div>
+            <div className="pagination">
+                <Stack spacing={2}>
+                    <Pagination count={18} shape="rounded" />
+                </Stack>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,6 @@ import Header from '../Header/Header';
 import BooksDisplay from '../BooksDisplay/BooksDisplay';
 import Footer from '../Footer/Footer';
 import UserServices from '../../Services/UserService';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
-
 
 const obj = new UserServices();
 
@@ -40,11 +37,6 @@ export class Home extends Component {
             <div>
                 <Header/>
                 <BooksDisplay bookarr = {this.state.bookarr} booksDisplay={this.booksDisplay}/>
-                <div className="pagination">
-                    <Stack spacing={2}>
-                        <Pagination count={18} shape="rounded" />
-                    </Stack>
-                </div>
                 <Footer/>
             </div>
         )
